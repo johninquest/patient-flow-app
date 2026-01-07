a371b56a23144e56e290939a1e78c480bcb3a741
john.dev <johninquest@gmail.com>
2026-01-07 11:14:48 +0100

    attempt #1 to fix pwa issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,26 +17,26 @@ export default defineConfig({
             selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
             manifest: {
                 short_name: 'Popaty',
-                name: 'Popaty - Property Management',
+                name: 'Popaty - Property rent and expense manager',
                 start_url: '/',
                 scope: '/',
                 display: 'standalone',
                 theme_color: '#8c6a3a',
                 background_color: '#F9FAFB',
-                description: 'Property rent and expense management application',
+                description: 'Property rent and expense management app',
                 icons: [
                     {
-                        src: '/pwa-192x192.png',
+                        src: '/icon-192.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: '/pwa-512x512.png',
+                        src: '/icon-512.png',
                         sizes: '512x512',
                         type: 'image/png'
                     },
                     {
-                        src: '/pwa-512x512.png',
+                        src: '/icon-512-maskable.png',
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: 'any maskable'
