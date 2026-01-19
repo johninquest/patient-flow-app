be84e89205d53ee6481d66a96c37e3c050581a17
john.dev <johninquest@gmail.com>
2026-01-19 15:01:18 +0100

    incremented app version

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = 'Popaty';
-export const APP_VERSION = '0.1.15';
+export const APP_VERSION = '0.1.19';
 
 export const SUPPORTED_LOCALES = [
     { code: 'en', name: 'English' },
