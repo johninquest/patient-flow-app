b1151ef5354c01b8d8e1c9cf7c5246b31247a2ed
john.dev <johninquest@gmail.com>
2026-01-19 14:59:14 +0100

    added posthog to client

--- client/src/routes/+layout.ts
+++ client/src/routes/+layout.ts
@@ -1,1 +1,18 @@
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
 export const ssr = false;
+
+export const load = async () => {
+  if (browser) {
+    posthog.init(
+      'phc_Y1X2kpbv6lPYA8zmpr2RQuYR8NWmygpIZuuZXGKNkmj',
+      {
+        api_host: 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only',
+      }
+    )
+  }
+
+  return {};
+};
